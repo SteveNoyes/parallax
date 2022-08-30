2670e1b0a53407777b850a408b4a37bd06986e38
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-08-29 21:07:31 -0400

    background jump fixed

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 // setting initial speed
 let gameSpeed = 5;
-let gameFrame = 0;
+
 // setting background layers to background images
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = '/img/layer-1.png'
@@ -19,70 +19,71 @@ const backgroundLayer4 = new Image();
 backgroundLayer4.src = '/img/layer-4.png'
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = '/img/layer-5.png'
-// set slide to gameSpeed set on line 9
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-// display game speed on html page
-const showGameSpeed = document.getElementById('showGameSpeed');
-showGameSpeed.innerHTML = gameSpeed;
-// when the user changes slide position update game speed
-slider.addEventListener('change', function(x){
-  gameSpeed = x.target.value;
-  showGameSpeed.innerHTML = x.target.value;
-})
-// class for building each layer object and setting its speed
-class Layer {
-  constructor(image, speedModifier) {
-    this.x = 0;
-    this.y = 0;
-    this.width = 2400;
-    this.height = 700;
-    this.image = image;
-    this.speedModifier = speedModifier;
-    this.speed = gameSpeed * this.speedModifier;
-  }
-  // update speed and frame rate
-  update() {
-    this.speed = gameSpeed * this.speedModifier;
-    this.x = gameFrame * this.speed % this.width;
-  }
-  // set image location
-  draw() { // two images are drawn, the second 'moved over' the width of the image
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-  }
-};
-// create objects using the class on line 34
-// passing in image from 12 - 21 and and the
-// number to be multipied by gameSpeed
-// these are the constants that will be multipied by
-// the relative gameSpeed that is set by user
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
-// putting all the layers into an array
-// so it can be accessed by the forEach, line 73
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-
-function animate() {
-  // this clears the previous image on screen so it doesn't 'smear' across the page
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-  gameObjects.forEach(object => {
-    // for each item in the array update and draw their current location
-    // every time this runs
-    object.update();
-    object.draw();
+// run once window has loaded
+window.addEventListener('load', function() {
+  // set slide to gameSpeed set on line 9
+  const slider = document.getElementById('slider');
+  slider.value = gameSpeed;
+  // display game speed on html page
+  const showGameSpeed = document.getElementById('showGameSpeed');
+  showGameSpeed.innerHTML = gameSpeed;
+  // when the user changes slide position update game speed
+  slider.addEventListener('change', function(x){
+    gameSpeed = x.target.value;
+    showGameSpeed.innerHTML = x.target.value;
   })
-  // setting frame rate to decrement will move the frame right to left
-  // i would have to change more than just this but if gameFrame is set to
-  // increment each iteration the frame will move left to right.
-  // (or 'backwards' relative to users gameplay)
-  gameFrame--;
-  // recursively call animate
-  requestAnimationFrame(animate);
-};
-// start
-animate();
+  // class for building each layer object and setting its speed
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
+    // update speed and frame rate
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      if(this.x <= -this.width) {
+        this.x = 0;
+      }
+      this.x = this.x - this.speed;
+    }
+    // set image location
+    draw() { // two images are drawn, the second 'moved over' the width of the image
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    }
+  };
+  // create objects using the class on line 36
+  // passing in image from 12 - 21 and and the
+  // number to be multipied by gameSpeed
+  // these are the constants that will be multipied by
+  // the relative gameSpeed that is set by user
+  const layer1 = new Layer(backgroundLayer1, 0.2);
+  const layer2 = new Layer(backgroundLayer2, 0.4);
+  const layer3 = new Layer(backgroundLayer3, 0.6);
+  const layer4 = new Layer(backgroundLayer4, 0.8);
+  const layer5 = new Layer(backgroundLayer5, 1);
+  // putting all the layers into an array
+  // so it can be accessed by the forEach, line 78
+  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
+  function animate() {
+    // this clears the previous image on screen so it doesn't 'smear' across the page
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    gameObjects.forEach(object => {
+      // for each item in the array update and draw their current location
+      // every time this runs
+      object.update();
+      object.draw();
+    })
+    // recursively call animate
+    requestAnimationFrame(animate);
+  };
+  // start
+  animate();
+});
